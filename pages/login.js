4379d32f0cb830a5f97b5f04ed7e058a9a46eefd
Romeo Colangelo <romeo@colangelo.me>
2023-05-18 22:02:01 +0200

    revert

--- pages/login.js
+++ pages/login.js
@@ -1,45 +1,47 @@
-import {
-  useAddress,
-  useMetamask,
-  useUser,
-  useLogin,
+import{
+    useAddress,
+    useMetamask,
+    useUser,
+    useLogin,
 } from "@thirdweb-dev/react";
 
 import styles from "../styles/Home.module.css";
 
-export default function Login() {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
 
-  //Login
-  const { login } = useLogin();
-  const { user } = useUser();
+export default function Login(){
+    const address = useAddress();    
+    const connectWithMetamask = useMetamask();
+    
+    //Login
+    const {login} = useLogin();
+    const {user} = useUser();
 
-  return (
+
+    return(
     <div className={styles.container}>
-      {address ? (
-        <>
-          <p> Welcome, {address.slice(0, 6)} ... </p>
+        {address ? (
+            <>
+            <p> Welcome, {address.slice(0, 6 )} ... </p>
 
-          <button
-            className={styles.mainButton}
-            style={{ width: 256 }}
-            onClick={login}
-          >
-            Login
-          </button>
-        </>
-      ) : (
-        <>
-          <button
-            className={styles.mainButton}
-            style={{ width: "fit-content", padding: 16, paddingLeft: 16 }}
+            <button
+                className={styles.mainButton}
+                style={{width :256}}
+                onClick={login()}
+            >
+                Login
+            </button>
+            </>
+        ) : ( 
+            <>
+            <button className={styles.mainButton}
+            style={{width:"fit-content", padding: 16, paddingLeft: 16}}
             onClick={() => connectWithMetamask()}
-          >
+            >
             Connect Wallet
-          </button>
-        </>
-      )}
-    </div>
-  );
-}
+            </button>
+            </>
+        )}
+  </div>
+
+    );
+};
